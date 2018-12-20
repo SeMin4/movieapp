e5740f8506e617bc568e556079d4ee9328373a47
SeMin <ohsemin4@gmail.com>
2018-12-20 23:24:11 +0900

    csv file

--- app/src/main/java/com/example/semin/movieapp/MainActivity.java
+++ app/src/main/java/com/example/semin/movieapp/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.InputStream;
 import java.sql.BatchUpdateException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     Button mypageBtn, loginBtn;
@@ -15,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InputStream inputStream = getResources().openRawResource(R.raw.earthquake_shelter);
+
+        Scanner scanner = new Scanner(inputStream);
+        ArrayList<EarthquakeShelter> list = new ArrayList<>();
+
+        scanner.nextLine();
+        while(scanner.hasNextLine()){
+            String[] data = scanner.nextLine().split(",");
+            try {
+                list.add(new EarthquakeShelter(data[2], Double.parseDouble(data[6]), Double.parseDouble(data[5])));
+            }catch(NumberFormatException e){
+                continue;
+            }catch(ArrayIndexOutOfBoundsException e){
+                continue;
+            }
+        }
+        scanner.close();
+
         mypageBtn = (Button)findViewById(R.id.main_my_page);
         loginBtn =(Button)findViewById(R.id.main_login);
         mypageBtn.setOnClickListener(new View.OnClickListener() {
